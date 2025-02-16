e340adeebc02e1b1ef11d3bd7e0d39a2e95166b0
jschomchoey <tpcc.general@gmail.com>
2025-02-17 02:08:54 +0700

    Update

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: "findmate-react.vercel.app", // React app URL
+    origin: "https://findmate-react.vercel.app", // Added https://
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
