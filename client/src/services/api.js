b80bd371de3f1b632e27385cbc5cb2003e9a6bf7
jschomchoey <tpcc.general@gmail.com>
2025-02-17 00:14:14 +0700

    client deploy

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "findmate-react-production.up.railway.app",
+  baseURL: "https://findmate-react-production.up.railway.app",
 });
 
 export default api;
