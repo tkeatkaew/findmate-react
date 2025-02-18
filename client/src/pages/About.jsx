1bb3f5e97b567326dad230d663401d3f131d1962
jschomchoey <tpcc.general@gmail.com>
2025-02-19 06:47:32 +0700

    add donate

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -185,7 +185,8 @@ const AboutPage = () => {
     {
       name: "Teerapat Chomchoey",
       role: "Full Stack Developer",
-      image: James,
+      image:
+        "https://scontent.fbkk13-1.fna.fbcdn.net/v/t39.30808-6/434360523_2038464119861737_8956847562164730460_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=XlgyEISrINgQ7kNvgGvosb9&_nc_oc=Adj_k_6ZXbFD86DjER_qy6Olr-_8eBo20pcF3uSm9p42Pdv2dckUlDdea_pYw3q_Kj0&_nc_zt=23&_nc_ht=scontent.fbkk13-1.fna&_nc_gid=ACqBSHcYKH8Jp1dxB87QtOH&oh=00_AYBr4tA5JpBnJW8cec8lyOgpf5_bPakeMgvVFMBRisrk6g&oe=67A10406",
       bio: "Hello! I am a Full Stack Developer with a strong focus on crafting seamless user experiences and robust backend systems. I love building applications that solve real-world problems.",
       education: "Rajamangala University of Technology Lanna Chiang Mai",
       links: [
@@ -214,7 +215,8 @@ const AboutPage = () => {
     {
       name: "Jittapong Jongjai",
       role: "Frontend Developer",
-      image: Toon,
+      image:
+        "https://scontent.fbkk12-4.fna.fbcdn.net/v/t39.30808-1/407671912_3114387742029699_3195186485123866947_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=103&ccb=1-7&_nc_sid=e99d92&_nc_ohc=514wNeVpEEYQ7kNvgF1u0RB&_nc_oc=Adh0xT3_2IPkPBX6Iq_rFrjyK3m-e6I5cIHp4SLuQABwrsRRnvqsjOvkCybRwdZKWY0&_nc_zt=24&_nc_ht=scontent.fbkk12-4.fna&_nc_gid=A9QQYJHosksz2YdI-ksEhRp&oh=00_AYByTtG5c-Q-jby_jYuhz1Uw_QkAI1DHlBGuprbp8DaqkA&oe=67A0EEF9",
       bio: "An enthusiastic Frontend Developer passionate about creating beautiful and intuitive user interfaces. I specialize in React and modern web technologies.",
       education: "Rajamangala University of Technology Lanna Chiang Mai",
       links: [
