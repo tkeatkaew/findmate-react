7df6de02ce76025adc22d4a100f2c717d8edbc1c
jschomchoey <tpcc.general@gmail.com>
2025-02-19 19:31:43 +0700

    preview image form

--- client/src/pages/PersonalInfo.jsx
+++ client/src/pages/PersonalInfo.jsx
@@ -892,7 +892,7 @@ const PersonalInfo = () => {
           maxWidth: "600px",
           minWidth: "400px",
           margin: "auto",
-          marginTop: "10vh",
+          marginTop: "2rem",
           border: "1px solid #eee",
           boxShadow: "0 2px 10px rgba(0, 0, 0, 0.08)",
           borderRadius: "20px",
