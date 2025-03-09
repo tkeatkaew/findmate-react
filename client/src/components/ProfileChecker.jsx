20f03e047de32d72bf23577e262a9a17ace77007
jschomchoey <tpcc.general@gmail.com>
2025-03-10 00:28:42 +0700

    warning popup

--- client/src/components/ProfileChecker.jsx
+++ client/src/components/ProfileChecker.jsx
@@ -73,7 +73,7 @@ const ProfileChecker = ({ children }) => {
             } else if (hasIncompleteTraits) {
               navigate("/personalityprofile", { state: { user_id: user.id } });
             }
-          }, 50);
+          }, 1);
         }
       } catch (error) {
         console.error("Error checking profile:", error);
